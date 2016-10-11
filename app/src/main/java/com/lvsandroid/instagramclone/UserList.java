1b6de98d285ddacc70c473952859f97bc266a191
lvs <lvs.freelance@gmail.com>
2016-10-11 17:18:41 +0700

    External storage permissions ok

--- app/src/main/java/com/lvsandroid/instagramclone/UserList.java
+++ app/src/main/java/com/lvsandroid/instagramclone/UserList.java
@@ -1,13 +1,23 @@
 package com.lvsandroid.instagramclone;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
@@ -15,9 +25,14 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class UserList extends AppCompatActivity {
 
     ArrayAdapter<String> arrayAdapter;
@@ -59,6 +74,58 @@ public class UserList extends AppCompatActivity {
                 });
         for(String usr : userNames) {
             Log.i("users list",usr);
+        }
+    }
+
+    //=== Menu ===
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.share) {
+            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i,1);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //=== Photo import ===
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            try {
+                if (ActivityCompat.checkSelfPermission(UserList.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                {
+                    UserListPermissionsDispatcher.onActivityResultWithCheck(this,requestCode,resultCode,data);
+                } else {
+
+                    Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    //ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                    //imageView.setImageBitmap(bitmapImage);
+                }
+
+
+            } catch (Exception e) {
+                Log.e("ERROR",e.toString());
+            }
         }
     }
 }
